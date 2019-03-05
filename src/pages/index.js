1b3303c09b70ded27ee65399eedce7c06803ef77
strangehill <strangehill@antarg.co>
2019-03-05 16:52:06 +0800

    gatsby-starer-blog-with-styled-components-and-yida-labeling

---
+++ src/pages/index.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import styled from 'styled-components'
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
+
+const Title = styled.h3`
+  margin-bottom: ${rhythm(1 / 4)};
+`
+const StyledLink = styled(Link)`
+  box-shadow: 'none';
+`
+
+class BlogIndex extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title="All posts"
+          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        />
+        <Bio />
+        {posts.map(({ node }) => {
+          const title = node.frontmatter.title || node.fields.slug
+          return (
+            <div key={node.fields.slug}>
+              <Title>
+                <StyledLink to={node.fields.slug}>
+                  {title}
+                </StyledLink>
+              </Title>
+              <small>{node.frontmatter.date}</small>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </div>
+          )
+        })}
+      </Layout>
+    )
+  }
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
